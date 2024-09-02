cbdffe632197a9c0a7d261a1e3c2998bd0f0f7bb
foxx <jkoontsiii@gmail.com>
2024-09-01 20:12:20 -0400

    .

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/sh
+# shellcheck shell=dash
+# shellcheck disable=SC2039
 git clone https://github.com/helloimalemur/words;
-cd words/;
+cd words/ || exit;
 pnpm install;
 export NO_STRIP=true;
 pnpm tauri build;
