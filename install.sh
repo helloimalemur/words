b7d04ed1a0fb5f9d39b05a14054792cb7c6d4784
foxx <jkoontsiii@gmail.com>
2024-09-01 20:10:00 -0400

    .

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
-git clone https://github.com/helloimalemur/words
-cd words/
-pnpm install
-export NO_STRIP=true
-pnpm tauri build
+#!/bin/bash
+git clone https://github.com/helloimalemur/words;
+cd words/;
+pnpm install;
+export NO_STRIP=true;
+pnpm tauri build;
