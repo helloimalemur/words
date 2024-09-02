b0563061f11cd65730c5a4115594dbc1316dab94
foxx <jkoontsiii@gmail.com>
2024-09-01 20:07:38 -0400

    .

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 git clone https://github.com/helloimalemur/words
-cd words/ || echo "clone failed, dir missing" && exit 1
+cd words/
 pnpm install
 export NO_STRIP=true
 pnpm tauri build
